fca0803aa1830b3e676d2a22b970d4a6f3cd3941
Chantee Smith <chantee@Elysium.attlocal.net>
2019-06-21 20:03:32 -0500

    Recipe Exercise Week 1

---
+++ script.js
@@ -0,0 +1,69 @@
+
+// Question 1
+
+// function marinaraSauce(){
+
+//     console.log("Heat a medium-large saucepan over medium heat.");
+//     console.log("Add 2 tspns of oil and 5 garlic cloves, cook until golden, about 2 minutes");
+//     console.log("Add 1/4 cup water, 2 cans of crushed tomatoes, and salt and season with black pepper to taste.")
+//     console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes. ")
+//     console.log("Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.")
+    
+//     }
+    
+//     marinaraSauce()
+
+
+
+// Question 2
+
+
+// function marinaraSauce (cookName) {
+//     console.log("Hey," + cookName + "! This is my mainara sauce recipe!");
+// }
+
+// marinaraSauce("Adam");
+
+
+
+// Question 3
+
+
+// var two = 2;
+// var oneFourth = "1/4"
+
+// function marinaraSauce(){
+
+// console.log("Heat a medium-large saucepan over medium heat.");
+// console.log("Add " + two + " tspns of oil and 5 garlic cloves, cook until golden, about " + two + " minutes");
+// console.log("Add " + oneFourth + " cup water, " + two + " cans of crushed tomatoes, and salt and season with black pepper to taste.")
+// console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes. ")
+// console.log("Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.")
+
+// }
+
+
+
+// marinaraSauce()
+
+
+// Combined Questions
+
+var two = 2;
+var oneFourth = "1/4"
+
+function marinaraSauce(cookName){
+
+console.log("Hey, " + cookName + "! This is my mainara sauce recipe!");    
+
+console.log("Heat a medium-large saucepan over medium heat.");
+console.log("Add " + two + " tspns of oil and 5 garlic cloves, cook until golden, about " + two + " minutes.");
+console.log("Add " + oneFourth + " cup water, " + two + " cans of crushed tomatoes, and salt and season with black pepper to taste.")
+console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes. ")
+console.log("Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.")
+
+}
+
+
+
+marinaraSauce("Adam");
